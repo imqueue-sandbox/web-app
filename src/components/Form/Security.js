ca2212ecb79d4a2593d417523164eb210064ad49
Mykhailo Stadnyk <mikhus@gmail.com>
2018-10-30 11:31:15 +0200

    security panel wip

--- src/components/Form/Security.js
+++ src/components/Form/Security.js
@@ -17,18 +17,102 @@
  */
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
+import classNames from 'classnames';
 import { withStyles } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 const styles = theme => ({
-
+    textField: {
+        maxWidth: '30em',
+    },
+    passwordForm: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+    },
+    margin: {
+        margin: theme.spacing.unit,
+    },
 });
 
 export class Security extends Component {
-    state = {}
+    state = {
+        showOld: false,
+        showNew: false,
+        oldPassword: '',
+        newPassword: '',
+    }
+
+    showPassword = (which) => () => {
+        this.setState({ [`show${which}`]: !this.state[`show${which}`] });
+    }
+
+    handleChange = (which) => (event) => {
+        this.setState({ [which]: event.target.value });
+    }
 
     render() {
-        return <TextField />;
+        const { classes } = this.props;
+
+        return <div className={classes.passwordForm}>
+            <FormControl className={classNames(
+                classes.margin,
+                classes.textField
+            )}>
+                <InputLabel htmlFor="adornment-password">
+                    Old Password
+                </InputLabel>
+                <Input
+                    id="old-password"
+                    type={this.state.showOld ? 'text' : 'password'}
+                    value={this.state.oldPassword}
+                    onChange={this.handleChange('oldPassword')}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="Toggle password visibility"
+                                onClick={this.showPassword('Old')}
+                            >{this.state.showNew
+                                ? <Visibility />
+                                : <VisibilityOff />}
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                />
+            </FormControl>
+
+            <FormControl className={classNames(
+                classes.margin,
+                classes.textField
+            )}>
+                <InputLabel htmlFor="adornment-password">
+                    New Password
+                </InputLabel>
+                <Input
+                    id="new-password"
+                    type={this.state.showNew ? 'text' : 'password'}
+                    value={this.state.newPassword}
+                    onChange={this.handleChange('newPassword')}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="Toggle password visibility"
+                                onClick={this.showPassword('New')}
+                            >{this.state.showNew
+                                ? <Visibility />
+                                : <VisibilityOff />}
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                />
+            </FormControl>
+        </div>;
     }
 }
 
