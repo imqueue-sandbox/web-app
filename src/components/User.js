dd4f686bc4143ffb664faf6f2689241cbe7cbdf4
Mykhailo Stadnyk <mikhus@gmail.com>
2018-10-30 12:42:49 +0200

    user form wip

--- src/components/User.js
+++ src/components/User.js
@@ -19,18 +19,53 @@ import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { createFragmentContainer } from 'react-relay';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 import LinearProgress from "@material-ui/core/LinearProgress/LinearProgress";
+import { withStyles } from '@material-ui/core';
 import { AppMessage, Gravatar } from '.';
 import { AuthStorage } from '../common';
 import { CurrentUserFragment } from '../relay/queries';
 
+const styles = theme => ({
+    userBox: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+    },
+    userInfo: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+    },
+    stats: {
+        marginTop: '2em',
+    },
+    avatar: {
+        margin: '0 5em 0 3em !important',
+    },
+});
+
 export class User extends Component {
     state = {
         inProgress: true,
+        firstName: '',
+        lastName: '',
+        email: '',
+    }
+
+    constructor(props) {
+        super(props);
+        const { firstName, lastName, email } = props.data || {};
+        Object.assign(this.state, { firstName, lastName, email });
+    }
+
+    change = (what) => (event) => {
+        this.setState({ [what]: event.target.value });
     }
 
     render() {
-        const { error, data } = this.props;
+        const { error, data, classes } = this.props;
         const user = data;
 
         if (!user) {
@@ -46,24 +81,38 @@ export class User extends Component {
             return <AppMessage variant="error" message={error} />;
         }
 
-        return <div className="user-box">
-            <Gravatar user={user} size={100} editable/>
-            <div className="divider"/>
-            <div className="user-info">
-                <Typography>
-                    <b>Name:</b> {user.firstName} {user.lastName}
-                </Typography>
-                <Typography>
-                    <b>Email:</b> {user.email}
-                </Typography>
-                <Typography>
-                    <b>Cars in garage:</b> {user.carsCount}
-                </Typography>
-                <Typography>
-                    <b>Bookings made:</b> {0}
-                </Typography>
-                <Typography>
-                    <b>Cars washed:</b> {0}
+        return <div className={classes.userBox}>
+            <div className={classes.avatar}>
+                <Gravatar user={user} size={100} editable />
+            </div>
+            <div className={classes.userInfo}>
+                <div>
+                    <TextField
+                        id="first-name"
+                        label="First Name"
+                        value={this.state.firstName}
+                        onChange={this.change('firstName')}
+                        margin="normal"
+                    />
+                    <TextField
+                        id="last-name"
+                        label="Last Name"
+                        value={this.state.lastName}
+                        onChange={this.change('lastName')}
+                        margin="normal"
+                    />
+                </div>
+                <TextField
+                    id="email"
+                    label="Email"
+                    value={this.state.email}
+                    onChange={this.change('email')}
+                    margin="normal"
+                />
+                <Typography className={classes.stats}>
+                    <em>Cars in garage: {user.carsCount}</em> &nbsp;|&nbsp;
+                    <em>Bookings made: {0}</em> &nbsp;|&nbsp;
+                    <em>Cars washed: {0}</em>
                 </Typography>
             </div>
         </div>;
@@ -71,6 +120,7 @@ export class User extends Component {
 }
 
 User.propTypes = {
+    classes: PropTypes.object.isRequired,
     error: PropTypes.shape({
         message: PropTypes.string,
     }),
@@ -84,4 +134,5 @@ User.propTypes = {
     }),
 };
 
+User = withStyles(styles)(User);
 User = createFragmentContainer(User, CurrentUserFragment);
